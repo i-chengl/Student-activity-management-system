b486880f26bcc7933d01ac5c1eae77ed741ef95f
chenlin <ujschenlin@gmail.com>
2015-12-28 21:48:19 +0800

    modifiy conf

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
