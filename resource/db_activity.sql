80a9cffc58eef3c9f4d4e5dbb4f855037cafb9d0
i-chengl <lcq_s@sina.com>
2016-01-09 20:38:17 +0800

    数据库更新
    数据库设计2.1版本
    主键，外键设定待完善

--- resource/db_activity.sql
+++ resource/db_activity.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2016-01-08 11:00:07
+-- Generation Time: 2016-01-09 13:31:47
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -23,22 +23,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `act_activity`
+--
+
+CREATE TABLE IF NOT EXISTS `act_activity` (
+  `act_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '活动id',
+  `act_name` varchar(10) CHARACTER SET utf8 NOT NULL COMMENT '活动名称',
+  `act_date_beg` date NOT NULL COMMENT '活动起始日期',
+  `act_date_end` date NOT NULL COMMENT '活动结束日期',
+  `act_time_submit` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '活动提交时间',
+  `act_time_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '活动最后更新时间',
+  `act_is_personal` tinyint(1) NOT NULL COMMENT '是否个人活动(参与者可数)',
+  `act_id_submit` bigint(11) NOT NULL COMMENT '活动所有者(该活动表提交者)',
+  `act_host` varchar(80) CHARACTER SET utf8 DEFAULT NULL COMMENT '主办方',
+  `act_partici` text CHARACTER SET utf8 NOT NULL COMMENT '活动参与者(存储参与者id，函数分割/查询/统计)',
+  `act_id_cat` int(11) NOT NULL COMMENT '活动所属类别id',
+  `act_state` int(4) NOT NULL COMMENT '活动状态(待审核/审核未通过/完结)',
+  `act_attach` varchar(80) CHARACTER SET utf8 DEFAULT NULL COMMENT '附件',
+  `act_comment` text CHARACTER SET utf8 COMMENT '备注',
+  `act_detail` text CHARACTER SET utf8mb4 NOT NULL COMMENT '详情(指导老师，描述，介绍，总结...)',
+  PRIMARY KEY (`act_id`),
+  KEY `id_submit` (`act_id_submit`) COMMENT '外键约束',
+  KEY `id_cat` (`act_id_cat`) COMMENT '外键约束'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='活动表' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `act_admin`
 --
 
 CREATE TABLE IF NOT EXISTS `act_admin` (
-  `admin_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '管理员id',
-  `admin_name` varchar(20) NOT NULL COMMENT '账号',
-  `admin_pass` varchar(20) NOT NULL COMMENT '密码',
-  PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- 转存表中的数据 `act_admin`
---
-
-INSERT INTO `act_admin` (`admin_id`, `admin_name`, `admin_pass`) VALUES
-(1, 'admin', 'admin');
+  `adm_id` bigint(11) unsigned NOT NULL COMMENT 'id',
+  `adm_name` varchar(10) NOT NULL COMMENT '姓名',
+  `adm_passwd` varchar(20) NOT NULL COMMENT '密码',
+  PRIMARY KEY (`adm_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员表';
 
 -- --------------------------------------------------------
 
@@ -47,108 +67,12 @@ INSERT INTO `act_admin` (`admin_id`, `admin_name`, `admin_pass`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `act_category` (
-  `cat_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '活动类别ID',
-  `cat_name` varchar(20) NOT NULL COMMENT '活动类别名',
-  `has_child` tinyint(1) NOT NULL DEFAULT '0' COMMENT '有无子类',
-  PRIMARY KEY (`cat_id`),
-  UNIQUE KEY `cat_name` (`cat_name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
-
---
--- 转存表中的数据 `act_category`
---
-
-INSERT INTO `act_category` (`cat_id`, `cat_name`, `has_child`) VALUES
-(1, '学术竞赛', 1),
-(2, '社会实践', 0),
-(3, '科研立项', 0),
-(4, '学习指导', 1),
-(5, '团日活动', 0),
-(6, '其他', 0);
-
--- --------------------------------------------------------
-
---
--- 表的结构 `act_detail`
---
-
-CREATE TABLE IF NOT EXISTS `act_detail` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '活动ID',
-  `name` varchar(40) NOT NULL COMMENT '活动名称',
-  `manger_id` int(11) NOT NULL COMMENT '活动组织者id',
-  `cat_id` int(11) NOT NULL COMMENT '分类id',
-  `cat_sub_id` int(11) DEFAULT NULL COMMENT '子类id',
-  `submit_time` datetime NOT NULL COMMENT '提交时间',
-  `update_time` datetime DEFAULT NULL COMMENT '最近更新时间',
-  `beg_date` date NOT NULL COMMENT '活动开始时间',
-  `end_time` date NOT NULL COMMENT '活动结束时间',
-  `detail_add_id` int(11) DEFAULT NULL COMMENT '活动详情附加项id',
-  `status` int(1) NOT NULL COMMENT '活动状态（0,待审核1,通过2,未通过3.完结）',
-  `is_personal` tinyint(1) DEFAULT NULL COMMENT '是否个人活动',
-  `count_participants` int(10) NOT NULL COMMENT '参与者人数',
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`manger_id`) COMMENT '用户id',
-  KEY `cat_id` (`cat_id`) COMMENT '分类id外键',
-  KEY `add_id` (`detail_add_id`) COMMENT '附加项id'
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
-
---
--- 转存表中的数据 `act_detail`
---
-
-INSERT INTO `act_detail` (`id`, `name`, `manger_id`, `cat_id`, `cat_sub_id`, `submit_time`, `update_time`, `beg_date`, `end_time`, `detail_add_id`, `status`, `is_personal`, `count_participants`) VALUES
-(1, '基于web的院系监考安排发布系统', 2, 1, 1, '2013-01-08 00:00:00', NULL, '2013-01-08', '2013-01-08', NULL, 0, 1, 1),
-(2, '节能环保，倡导低碳生活', 5, 2, NULL, '2016-01-07 00:00:00', NULL, '2016-01-14', '2016-01-22', 1, 0, 1, 1),
-(3, '江苏高校传媒联盟新闻奖三等奖 ', 7, 6, NULL, '2013-01-08 00:00:00', NULL, '2013-03-05', '2013-03-08', 2, 3, 1, 1),
-(4, '江苏大学第11批大学生科研立项资助项目', 8, 3, NULL, '2012-01-13 00:00:00', NULL, '2011-01-13', '2012-01-13', NULL, 3, 1, 1);
-
--- --------------------------------------------------------
-
---
--- 表的结构 `act_detail_add`
---
-
-CREATE TABLE IF NOT EXISTS `act_detail_add` (
-  `det_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '活动详情id',
-  `techer_id` varchar(8) DEFAULT NULL COMMENT '指导老师',
-  `summary` text NOT NULL COMMENT '活动总结',
-  `participants` text COMMENT '参与者详情',
-  `attachment` int(11) DEFAULT NULL COMMENT '附件',
-  `comment` text COMMENT '备注',
-  PRIMARY KEY (`det_id`),
-  KEY `teacher_id` (`techer_id`) COMMENT '指导老师id'
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- 转存表中的数据 `act_detail_add`
---
-
-INSERT INTO `act_detail_add` (`det_id`, `techer_id`, `summary`, `participants`, `attachment`, `comment`) VALUES
-(1, NULL, '', '', 0, NULL),
-(2, NULL, '新闻奖三等奖', '', 0, NULL);
-
--- --------------------------------------------------------
-
---
--- 表的结构 `act_sub_category`
---
-
-CREATE TABLE IF NOT EXISTS `act_sub_category` (
-  `sub_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '子类ID',
-  `sub_name` varchar(20) NOT NULL COMMENT '子类名字',
-  `parent_id` int(11) DEFAULT NULL COMMENT '父类ID',
-  PRIMARY KEY (`sub_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
-
---
--- 转存表中的数据 `act_sub_category`
---
-
-INSERT INTO `act_sub_category` (`sub_id`, `sub_name`, `parent_id`) VALUES
-(1, '校内竞赛', 1),
-(2, '校外竞赛', 1),
-(3, '入学教育', 4),
-(4, '就业指导', 4);
+  `cat_id` bigint(11) unsigned NOT NULL COMMENT '活动类别id',
+  `cat_name` varchar(40) NOT NULL COMMENT '活动类别名称',
+  `cat_level` int(4) NOT NULL COMMENT '类别深度(1，2...级分类)',
+  `cat_id_parent` bigint(11) DEFAULT NULL COMMENT '父类别id(level为1时此处为NULL)',
+  PRIMARY KEY (`cat_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='活动类别表';
 
 -- --------------------------------------------------------
 
@@ -157,42 +81,14 @@ INSERT INTO `act_sub_category` (`sub_id`, `sub_name`, `parent_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `act_user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `user_name` varchar(20) NOT NULL COMMENT '用户名',
-  `user_pass` varchar(20) NOT NULL COMMENT '用户密码',
-  `is_teacher` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否老师',
-  `user_Email` varchar(40) DEFAULT NULL COMMENT '邮箱',
-  `user_depart` text NOT NULL COMMENT '学院',
-  `user_class` text NOT NULL COMMENT '班级',
-  `user_num` bigint(20) NOT NULL COMMENT '学号/工号',
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_num` (`user_num`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
-
---
--- 转存表中的数据 `act_user`
---
-
-INSERT INTO `act_user` (`user_id`, `user_name`, `user_pass`, `is_teacher`, `user_Email`, `user_depart`, `user_class`, `user_num`) VALUES
-(1, 'chenlin', 'admin', 0, '615362333@qq.com', 'Computer science and communication engineering institute', '1201 computer science and technology ', 3120602019),
-(2, '孙亚楠', '12345', 0, NULL, '计算机科学与通信工程学院', '', 3120601002),
-(4, '潘雨清', '12345', 1, NULL, '计算机科学与通信工程学院', '', 1000000000),
-(5, '马悦峰', '12345', 0, NULL, '计算机科学与通信工程学院', '', 3120601001),
-(6, '马学文', '12345', 1, NULL, '计算机科学与通信工程学院', '', 1000000001),
-(7, '王丽珍', '12345', 0, NULL, '计算机科学与通信工程学院', '计算机1201', 3120602001),
-(8, '李露', '12345', 0, NULL, '计算机科学与通信工程学院', '计算机1001', 3100602001);
-
---
--- 限制导出的表
---
-
---
--- 限制表 `act_detail`
---
-ALTER TABLE `act_detail`
-  ADD CONSTRAINT `detail_id` FOREIGN KEY (`detail_add_id`) REFERENCES `act_detail_add` (`det_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `cat_id` FOREIGN KEY (`cat_id`) REFERENCES `act_category` (`cat_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_id` FOREIGN KEY (`manger_id`) REFERENCES `act_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  `usr_id` bigint(11) unsigned NOT NULL COMMENT '学号/工号',
+  `usr_name` varchar(10) NOT NULL COMMENT '用户名',
+  `usr_passwd` varchar(20) NOT NULL COMMENT '密码',
+  `usr_state` tinyint(1) NOT NULL COMMENT '身份：1-teacher 0-student',
+  `usr_depart` varchar(40) DEFAULT NULL COMMENT '所属院系(教师可为NULL)',
+  `usr_class` varchar(20) DEFAULT NULL COMMENT '所属班级(教师为NULL)',
+  PRIMARY KEY (`usr_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
