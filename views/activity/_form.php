ffd7a51e60a7b30ff1b544061e10b4e485f4306e
chenlin <ujschenlin@gmail.com>
2016-01-11 20:52:00 +0800

    增加数据库关联（view层）

--- views/activity/_form.php
+++ views/activity/_form.php
@@ -2,7 +2,11 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\jui\DatePicker;
+use app\models\User;
+use app\models\Category;
 
+$datePicker = new DatePicker();
 /* @var $this yii\web\View */
 /* @var $model app\models\Activity */
 /* @var $form yii\widgets\ActiveForm */
@@ -14,25 +18,29 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'act_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'act_date_beg')->textInput() ?>
+    <?= $form->field($model, 'act_date_beg')->textInput()//->widget($datePicker) ?>
 
     <?= $form->field($model, 'act_date_end')->textInput() ?>
 
-    <?= $form->field($model, 'act_time_submit')->textInput() ?>
+    <?//= $form->field($model, 'act_time_submit')->textInput() ?>
 
-    <?= $form->field($model, 'act_time_update')->textInput() ?>
+    <?//= $form->field($model, 'act_time_update')->textInput() ?>
 
-    <?= $form->field($model, 'act_is_personal')->textInput() ?>
+    <?= $form->field($model, 'act_is_personal')//->textInput()
+		->dropDownList(['2'=>' ','1'=>'是','0'=>'否'])?>
 
-    <?= $form->field($model, 'act_id_submit')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'act_id_submit')//->textInput(['maxlength' => true])
+    	->dropDownList(User::find()->select(['usr_name','usr_id'])->indexBy('usr_id')->column()) ?>
 
     <?= $form->field($model, 'act_host')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'act_partici')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'act_id_cat')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'act_id_cat')//->textInput(['maxlength' => true])
+    	->dropDownList(Category::find()->select(['cat_name','cat_id'])->indexBy('cat_id')->column(),['id'=>'请选择活动分类']) ?>
 
-    <?= $form->field($model, 'act_state')->textInput() ?>
+    <?//= $form->field($model, 'act_state')->textInput()
+    	//->dropDownList(['','待审核','审核未通过','完结'])?>
 
     <?= $form->field($model, 'act_attach')->textInput(['maxlength' => true]) ?>
 
